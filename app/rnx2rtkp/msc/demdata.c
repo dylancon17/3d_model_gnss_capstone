09c262ee9e7fb4a762c084fdf4d929d1883699d2
Dylan Conley <dylanconley@shaw.ca>
2025-11-11 12:02:02 -0700

    Docstrings for DEM functions

--- app/rnx2rtkp/msc/demdata.c
+++ app/rnx2rtkp/msc/demdata.c
@@ -1,5 +1,12 @@
 #include "rtklib.h"
 
+
+/* set_relative_origin ---------------------------------------------------
+* set an origin to traverse along the DEM from
+* args   : DTMData   *DEM     I   DTM Object, see rtklib.h for definition and rnx2rtkp.c for initial setup
+*          double    latitude I   latitude in radians
+*          double    longitudeI   longitude in radians
+* return : void (even if out of bounds, that will be handled in get relative height call, but we can change this if you need)*/
 extern void set_relative_origin(
     struct DTMData* DEM,
     double latitude,
@@ -7,6 +14,14 @@ extern void set_relative_origin(
     return; //TODO-TC
 }
 
+/* get_relative_height ---------------------------------------------------
+* Report height for a location with respect to the relative origin
+* args   : DTMData   *DEM           I   DTM Object, see rtklib.h for definition and rnx2rtkp.c for initial setup
+*          int       *E I           I   Meters East from the relative origin (negative indicates West)
+*          int       *N             I   Meters North from the relative origin (negative indicates East)
+*          double    *h             I   The height of the DEM at that point
+*          int       *out_of_bounds I   1 if there is not DEM data at that point (h will not looked at if this is 1)
+* return : void (data returned by setting h and out_of_bounds)*/
 extern void get_relative_height(
     struct DTMData* DEM,
     int* E,
