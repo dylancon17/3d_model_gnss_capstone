f82bf4ed02b7b6f5f1d24009a5186a2d5c2dfb30
titusccastillon <113804469+tcastillonuofc@users.noreply.github.com>
2025-12-17 12:15:56 -0700

    Update out_of_bounds

--- app/rnx2rtkp/msc/DSM_data.c
+++ app/rnx2rtkp/msc/DSM_data.c
@@ -189,8 +189,8 @@ steps_XY calculate_steps_from_origin(const east_north* point, const DSMData* DSM
 /// @return True means that the point is outside the DSM bounds. False means that the point is within the bounds.
 int out_of_bounds_check(int x_steps, int y_steps, DSMData* DSM)
 {
-    double max_steps_x = DSM->n_columns;
-    double max_steps_y = DSM->n_rows;
+    int max_steps_x = DSM->n_columns;
+    int max_steps_y = DSM->n_rows;
     if (x_steps < 0 || y_steps < 0 || x_steps > max_steps_x || y_steps > max_steps_y) return 1;
     else return 0;
 }
